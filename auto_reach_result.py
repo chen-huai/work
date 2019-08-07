2c780ef72f3944c612f5270ae005646335e8dd2d
chenhuai <457794287@qq.com>
2019-08-07 10:28:46 +0800

    auto_reach 最终版

--- auto_reach_result.py
+++ auto_reach_result.py
@@ -16,11 +16,13 @@ pyautogui.PAUSE=1
 pyautogui.FAILSAFE= True 
 print(pyautogui.size())
 print(pyautogui.position())
-path=tkinter.filedialog.askopenfilenames(initialdir='Z:/Data/%s/66-01-2018-012 5110 ICP-OES/SVHC'%now_1,filetypes=[("file", "*.xls*")])
+
+path=tkinter.filedialog.askopenfilenames(initialdir='Z:/Data/%s/66-01-2018-012 5110 ICP-OES'%now_1,filetypes=[("file", "*.xls*")])
 excel = win32com.gencache.EnsureDispatch('Excel.Application')
 excel.Visible = True
 excel.Application.DisplayAlerts = True   
-workbook = excel.Workbooks.Open(os.path.join(os.getcwd(),r'%s'%path))
+workbook = openpyxl.load_workbook(os.path.join(os.getcwd(),'%s'%path))
+time.sleep(10)
 row=5
 x=117
 while excel.Sheets('SVHC').Cells(x,11).Value is not None:
@@ -30,10 +32,9 @@ for i in range(x-5):
     id_no=excel.Sheets("SVHC").Cells(row,1).Value
     list.append(result)
     id_number.append(id_no)
+    i+=1
     row+=1   
-fill_sample_number=float(input('Please fill in the sample number：'))
-m=id_number.index(fill_sample_number)
-time.sleep(10)
+m=0
 n=0
 while n in range(len(list)):
     if m+1<len(list):
@@ -50,11 +51,12 @@ while n in range(len(list)):
         if list[m]<0:
             list[m]=0 
         pyautogui.typewrite('%s'%list[m],0.0000000001)
-        pyautogui.typewrite(['Enter']) 
-        print('The next sample number：%s'%id_number[m+1])        
-        time.sleep(3)
+        pyautogui.typewrite(['down']) 
+        time.sleep(3)        
     else:
+        pyautogui.typewrite(['Enter'])
+        time.sleep(2)
         pyautogui.typewrite(['right']*6)
-        pyautogui.typewrite('%s'%id_number[m],0.0000000001)
+        pyautogui.typewrite('the end %s'%id_number[m],0.0000000001)
     m+=1
     n+=1
