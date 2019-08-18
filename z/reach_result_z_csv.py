04cc9e62548946cdffde5de31e7c76ff860980b5
chenhuai <457794287@qq.com>
2019-08-18 15:22:45 +0800

    reach result csv excel在2列时,同时添加超出结果的判定

--- z/reach_result_z_csv.py
+++ z/reach_result_z_csv.py
@@ -8,19 +8,20 @@ from win32com.client import Dispatch
 import win32com.client as win32com
 import random
 import csv
+import tkinter
 now_1 = str(time.strftime('%Y'))
 def get_list():  
     lab_number = input('please fill in the lab number:')
     quality=float(input('please fill in the sample quality:'))
-    volume=float(input('please fill in 体积:'))
+    volume=float(input('please fill in the volume of the sample:'))
     print ('begin')
     print ('Wait for a moment')
     name = lab_number.replace('/', '_') 
-    elements=['Zn','B','As','Sb','Cr','Pb','Co','Ba','Sn','Mo','Zr','Al','Cd','Na','Sr']
-    wavelenths=['202.548','249.772','188.980','206.834','267.716','220.353','228.615','230.424','189.925','202.032','343.823','236.705','228.802','589.592','460.733']
-    rows=['3','4','5','6','7','8','9','11','12','13','14','15','17','18','20']
-    n=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-    lines= csv.reader(open(path,"rt",encoding='utf-8'))
+    elements=['Zn','B','As','Sb','Cr','Pb','Co','Si','Ba','Sn','Mo','Zr','Al','Ti','Cd','Na','K','Sr','Ca','Mg']
+    #wavelenths=['202.548','249.772','188.980','206.834','267.716','220.353','228.615','230.424','189.925','202.032','343.823','236.705','228.802','589.592','460.733']
+    rows=['3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22']
+    n=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+    lines= csv.reader(open('%s'%path,"rt",encoding='utf-8'))
     lists=[]
     for l in lines:
         lists.append(l) 
@@ -28,26 +29,28 @@ def get_list():
     excel.Visible = True
     excel.Application.DisplayAlerts = True    
     workbook = excel.Workbooks.Open(os.path.join(os.getcwd(),r'./SVHC.xlsx'))
-    for i in range(15):
+    for i in range(len(elements)):
         list=[]
         m=0
         for line in lists:
             if ('%s'%lab_number in line[0]) and ('%s'%elements[i] in line[2]) and ('(ref)' not in line[2]):                         
                 list.append(line) 
             m+=1
-        excel.Sheets("1").Cells(rows[i],15).Value=elements[i]
+        excel.Sheets("1").Cells(rows[i],2).Value=elements[i]
         if list!=[]:
-            if str(list[0][4])=='未校正':
-                excel.Sheets("1").Cells(rows[i],16).Value='未校正'
+            if str(list[0][4])=='未校正' :
+                excel.Sheets("1").Cells(rows[i],3).Value='未校正'
+            elif str(list[0][4])=='####':
+                excel.Sheets("1").Cells(rows[i],3).Value='超出'
             else:
-                excel.Sheets("1").Cells(rows[i],16).Value=float(list[0][4])*volume/quality
+                excel.Sheets("1").Cells(rows[i],3).Value=float(list[0][4])*volume/quality
         else:
-            excel.Sheets("1").Cells(rows[i],16).Value='未走标准曲线'
-        if n[i]=='15':
-            workbook.SaveAs('.\\SVHC %s.xlsx'%name)
+            excel.Sheets("1").Cells(rows[i],3).Value='未走标准曲线'
+        if n[i]==n[len(elements)-1]:  
+            address=os.path.abspath('.')
+            workbook.SaveAs('%s\\SVHC %s.xlsx'%(address,name))
             excel.Application.Quit()
-        i+=1
-path=g.fileopenbox(msg=None,title="Select file",default='Z:/Data/%s/66-01-2018-012 5510 ICP-OES/'%now_1,filetypes=["*.csv"])
+path=tkinter.filedialog.askopenfilenames(initialdir='Z:/Data/%s/66-01-2018-012 5110 ICP-OES/'%now_1,filetypes=[("csvfile", "*.csv")])
 get_list()
 action=g.ccbox('whether need to run the program again', choices=('continue','finsh'))
 while  action==1:
