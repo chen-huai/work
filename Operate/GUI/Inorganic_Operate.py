5f1ee1fc99438ad7253316143c2dfcbc453cfc99
chen-fr <457794287@qq.com>
2021-06-08 15:01:22 +0800

    config文件配置编辑模式成功

--- Operate/GUI/Inorganic_Operate.py
+++ Operate/GUI/Inorganic_Operate.py
@@ -5,6 +5,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 import chicon  # 引用图标
 from Inorganic_Operate_Ui import *
+from Table_Ui import *
 
 class MyMainWindow(QMainWindow, Ui_MainWindow):
 	def __init__(self, parent=None):
@@ -48,6 +49,7 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
 		self.actionExport.triggered.connect(self.exportConfig)
 		self.actionImport.triggered.connect(self.importConfig)
 		self.actionExit.triggered.connect(MyMainWindow.close)
+		self.actionEdit.triggered.connect(self.showTable)
 		self.pushButton_37.clicked.connect(self.searchReachMessage)
 		self.actionImport.triggered.connect(self.lineEdit.clear)
 		self.actionHelp.triggered.connect(self.lineEdit.clear)
@@ -191,7 +193,13 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
 			if reply == QMessageBox.Yes:
 				MyMainWindow.createConfigContent(self)
 				MyMainWindow.getConfigContent(self)
-		self.lineEdit_6.setText("配置获取成功")
+		try:
+			self.lineEdit_6.setText("配置获取成功")
+		except AttributeError:
+			QMessageBox.information(self, "提示信息", "已获取配置文件内容", QMessageBox.Yes)
+		else:
+			pass
+
 
 	def createConfigContent(self):
 		configContent = [
@@ -2705,6 +2713,76 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
 		self.lineEdit.setText('Random')
 		self.lineEdit_6.setText("随时可以开始填写随机数")
 
+	def showTable(self):
+		myTable.createTable()
+		myTable.showMaximized()
+
+
+class MyTableWindow(QMainWindow, Ui_TableWindow):
+	def __init__(self, parent=None):
+		super(MyTableWindow, self).__init__(parent)
+		self.setupUi(self)
+		self.pushButton.clicked.connect(self.saveTable)
+		self.pushButton_2.clicked.connect(self.createTable)
+		# self.QtWidgets.QDialogButtonBox.Save
+
+
+	def createTable(self):
+		self.df = pd.read_csv('%s/config_inorganic.csv' % configFileUrl, names=['A', 'B', 'C'])
+		self.df_rows = self.df.shape[0]
+		self.df_cols = self.df.shape[1]
+		self.tableWidget.setRowCount(self.df_rows)
+		self.tableWidget.setColumnCount(self.df_cols)
+
+		# self.tabletWidget.
+		for i in range(self.df_rows):
+			for j in range(self.df_cols):
+				self.tableWidget.setItem(i, j, QTableWidgetItem(self.df.iloc[i, j]))
+		# 第1列不允许编辑
+		self.tableWidget.setItemDelegateForColumn(0,EmptyDelegate(self))
+		# 行颜色
+		self.tableWidget.setAlternatingRowColors(True)
+		# 显示所有内容
+		self.tableWidget.resizeColumnsToContents()
+		# 平均分配
+		self.tableWidget.horizontalHeader().setSectionResizeMode(True)
+
+	@pyqtSlot()
+	def print_my_df(self):
+		print(self.df)
+
+	@pyqtSlot()
+	def saveTable(self):
+		col = self.tableWidget.columnCount()
+		row = self.tableWidget.rowCount()
+		# for currentQTableWidgetItem in self.tableWidget.selectedItems():
+		# 	print((currentQTableWidgetItem.row(), currentQTableWidgetItem.column()))
+		data =[]
+		for i in range(col):
+			data.append(i)
+			data[i] = []
+			for j in range(row):
+				itemData = self.tableWidget.item(j,i).text()
+				data[i].append(itemData)
+		configFile = pd.DataFrame({'a': data[0], 'b': data[1], 'c': data[2]})
+		configFile.to_csv('%s/config_inorganic.csv' % configFileUrl,encoding="utf_8_sig", index=0, header=0)
+		reply = QMessageBox.question(self, '信息', '配置文件已修改成功，是否重新获取新的config文件内容',
+									 QMessageBox.Yes | QMessageBox.No,
+									 QMessageBox.Yes)
+		if reply == QMessageBox.Yes:
+			MyMainWindow.getConfigContent(self)
+
+# table不可编辑
+class EmptyDelegate(QItemDelegate):
+	def __init__(self, parent):
+		super(EmptyDelegate, self).__init__(parent)
+
+	def createEditor(self, QWidget, QStyleOptionViewItem, QModelIndex):
+		return None
+
+
+
+
 
 if __name__ == "__main__":
 	import sys
@@ -2721,6 +2799,7 @@ if __name__ == "__main__":
 	QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling)
 	app = QApplication(sys.argv)
 	myWin = MyMainWindow()
+	myTable = MyTableWindow()
 	myWin.show()
 	myWin.getConfig()
 	sys.exit(app.exec_())
